4ea89fe8ccc880d9faaeb33d047eb8a3c3e67c21
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2019-05-16 00:06:55 +0200

    Coverage

--- travis_build.sh
+++ travis_build.sh
@@ -3,3 +3,5 @@ set -e
 ./gradlew check publish
 mkdir -p report
 cp --parent */build/reports build/reports report -R
+java -jar ./codacy-coverage-reporter-6.0.0-assembly.jar report -l Java -r Jestures*/build/reports/jacoco/test/jacocoTestReport.xml
+
--- travis_pull_request.sh
+++ travis_pull_request.sh
@@ -3,3 +3,4 @@ set -e
 ./gradlew check artifactoryPublish
 mkdir -p report
 cp --parent */build/reports build/reports report -R
+java -jar ./codacy-coverage-reporter-6.0.0-assembly.jar report -l Java -r Jestures*/build/reports/jacoco/test/jacocoTestReport.xml
